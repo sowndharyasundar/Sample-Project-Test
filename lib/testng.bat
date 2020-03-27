178ca5b670b091b18abf015726ef4fa00bfae0f8
sowndharya <sowndharyasundar@gmail.com>
2020-03-27 17:23:27 +0100

    trying with add jar to lib

---
+++ lib/testng.bat
@@ -0,0 +1,5 @@
+set projectLocation = C:\Users\Sowndharya\workspace\SampleProjectTest
+cd %projectLocation%
+set classpath=%projectLocation%\bin;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\testSuite.xml
+pause
